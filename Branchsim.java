6fd1f6a11fbdaf3279f8cdf4e978e57d20dc8cdc
Caren Lai <carenlai0723@gmail.com>
2020-01-28 20:04:00 -0800

    Real first commit

---
+++ Branchsim.java
@@ -0,0 +1,14 @@
+public class Branchsim {
+
+    // Usage: java Branchsim [FILE_NAME] [M] [N]
+    public static void main(String[] args) {
+
+        // 1. Get (m,n) from args
+
+        // 2. Read the file and transform it
+
+        // 3. Build the predictor
+
+        // 4. "Run" the predictor with the trace
+    }
+}
