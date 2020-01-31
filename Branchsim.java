080bcb94728676c8f64d18a8ced4786b9afca8ec
Caren Lai <carenlai0723@gmail.com>
2020-01-30 16:59:34 -0800

    Add (m,n) predictor (correlation predictor)

--- Branchsim.java
+++ Branchsim.java
@@ -1,14 +1,145 @@
-public class Branchsim {
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
 
-    // Usage: java Branchsim [FILE_NAME] [M] [N]
-    public static void main(String[] args) {
+class GHistory {
+    private int bits;
+    private int mask;
+    private int value;
 
-        // 1. Get (m,n) from args
+    public GHistory(int bits) {
+        this.bits = bits;
+        this.mask = (int) (Math.pow(2, bits) - 1);
+        this.value = 0;
+    }
 
-        // 2. Read the file and transform it
+    public String getHistory() {
+        return Integer.toBinaryString(this.value);
+    }
 
-        // 3. Build the predictor
+    public String addHistory(String actual) {
+        this.value = (this.value << 1) & this.mask;
 
-        // 4. "Run" the predictor with the trace
+        if(actual.equals("T")){
+            this.value += 1;
+        }
+        return this.getHistory();
     }
 }
+
+public class Branchsim {
+
+    private static final String TAKEN = "T";
+    private static final String NOT_TAKEN = "N";
+
+    // 2-bit predictor
+    private static final String TAKEN_STRONG = "11";
+    private static final String TAKEN_WEAK = "10";
+    private static final String NOT_TAKEN_STRONG = "00";
+    private static final String NOT_TAKEN_WEAK = "01";
+
+    public static void main(String[] args) throws IOException {
+        // 1. read arguments (fileName, m, n, bits_to_index)
+        if(args.length < 4){
+            System.out.println("Usage: java Branchsim [FILE_NAME] [M_HISTORY_BIT] [N_BIT_PREDICTOR] [BITS_TO_INDEX]");
+            return;
+        }
+        String fileName = args[0];
+        int m = Integer.parseInt(args[1]);
+        int n = Integer.parseInt(args[2]);
+        int bitsToIndex = Integer.parseInt(args[3]);
+        if(m > 12 || bitsToIndex > 12 || n > 2 || n < 1) {
+            return;
+        }
+
+        // 2. Init the table and global history
+        GHistory history = new GHistory(m);
+        Map<String, String[]> BHT = initBranchHistoryTable(m, n, bitsToIndex);
+
+        // 3. Run the predictor
+        int total = 0;
+        int accurateCount = 0;
+        String trace;
+        BufferedReader br = new BufferedReader(new FileReader(fileName));
+        while ((trace = br.readLine()) != null) {
+            String[] tokens = trace.split(" ");
+            String address = tokens[0]; // "480558"
+            String actual = tokens[1];  // "T" or "N"
+
+            // 3.1 get the predictor
+            int key = getKeyByAddress(address, bitsToIndex);
+            String predictor = BHT.get(history.getHistory())[key];
+
+            // 3.2 do the prediction
+            String prediction =  predict(predictor);
+
+            // 3.3 do the statistics
+            total ++;
+            accurateCount += prediction.equals(actual) ? 1 : 0;
+
+            // 3.3 update the predictor and global history
+            String newState = updateTwoBitPredictor(predictor, actual);
+            String his = history.getHistory();
+            BHT.get(his)[key] = newState;
+            history.addHistory(actual);
+        }
+
+        float accuracy = (float) accurateCount / total;
+        System.out.println("accuracy: " + accuracy + "%");
+        return ;
+    }
+
+    // ("48bec6", 8) -> c6 (in int)
+    // ("48bec6", 4) -> 6 (in int)
+    private static int getKeyByAddress(String address, int bitsToIndex){
+        int mask = (int) (Math.pow(2, bitsToIndex) - 1);
+        return Integer.parseInt(address, 16) & mask;
+    }
+
+    private static String predict(String predictor){
+        if(predictor.equals(TAKEN_STRONG) || predictor.equals(TAKEN_WEAK)){
+            return TAKEN;
+        }else{
+            return NOT_TAKEN;
+        }
+    }
+
+    private static Map<String, String[]> initBranchHistoryTable(int m, int n, int bitsToIndex){
+        // m = 6, n = 2, bitsToIndex = 8
+        Map<String, String[]> BHT = new HashMap<>();
+        int max = (int) Math.pow(2, m);
+        for(int i = 0; i < max; i++){
+            String[] predictors = new String[(int)Math.pow(2, bitsToIndex)];
+            Arrays.fill(predictors, "00");
+
+            BHT.put(Integer.toBinaryString(i), predictors);
+        }
+        return BHT;
+    }
+
+    private static String updateTwoBitPredictor(String currentStatus, String actual) {
+        String state = "";
+        switch (currentStatus) {
+            case NOT_TAKEN_STRONG:
+                if (actual.equals(TAKEN)) state = NOT_TAKEN_WEAK;
+                else state = NOT_TAKEN_STRONG;
+                break;
+            case TAKEN_STRONG:
+                if (actual.equals(TAKEN)) state = TAKEN_STRONG;
+                else state = TAKEN_WEAK;
+                break;
+            case NOT_TAKEN_WEAK:
+                if (actual.equals(TAKEN)) state = TAKEN_STRONG;
+                else state = NOT_TAKEN_STRONG;
+                break;
+            case TAKEN_WEAK:
+                if (actual.equals(TAKEN)) state = TAKEN_STRONG;
+                else state = NOT_TAKEN_STRONG;
+                break;
+        }
+        return state;
+    }
+}
+
+//
